87bd1765ca01280930a4b9b9ada6660147d1befe
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-01-08 00:50:05 +0530

    Updates

---
+++ precache-manifest.43f93e83b8158d270215531887c1a3c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "59ae539eaead5107892e",
+    "url": "/Text-Summarizer/static/js/runtime~main.59ae539e.js"
+  },
+  {
+    "revision": "50c5c57cdfd2d5ea4db6",
+    "url": "/Text-Summarizer/static/js/main.50c5c57c.chunk.js"
+  },
+  {
+    "revision": "53292b73308187a15638",
+    "url": "/Text-Summarizer/static/js/1.53292b73.chunk.js"
+  },
+  {
+    "revision": "50c5c57cdfd2d5ea4db6",
+    "url": "/Text-Summarizer/static/css/main.d35b6687.chunk.css"
+  },
+  {
+    "revision": "53292b73308187a15638",
+    "url": "/Text-Summarizer/static/css/1.d1d6cc02.chunk.css"
+  },
+  {
+    "revision": "2603e1e34b83570d6beadd96a750231d",
+    "url": "/Text-Summarizer/index.html"
+  }
+];
