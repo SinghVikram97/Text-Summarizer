b0e98f2cbc1846db7cd024d005b3395163f19ba7
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-01-07 21:33:34 +0530

    Added number of sentences input

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React, {
-  Component
-} from 'react';
-import './App.css';
-import '../node_modules/tachyons'
-import Navbar from './Components/Navbar/Navbar'
-import InputArea from './Components/InputArea/InputArea'
-import SummarizedText from './Components/SummarizedText/SummarizedText'
-
+import React, { Component } from "react";
+import "./App.css";
+import "../node_modules/tachyons";
+import Navbar from "./Components/Navbar/Navbar";
+import InputArea from "./Components/InputArea/InputArea";
+import SummarizedText from "./Components/SummarizedText/SummarizedText";
 
 class App extends Component {
+  state = {
+    summary: ""
+  };
 
-  state={
-    summary:''
-  }
-
-  summarizeContent=(text)=>{
-    let summary=text.inputText;
+  summarizeContent = text => {
+    let summary = text.inputText;
+    let numSentences = text.numSentences;
+    console.log(numSentences);
     this.setState({
-      summary:summary
-    })
-  }
+      summary: summary
+    });
+  };
 
   render() {
-    return ( <div className = "App pl-0" >
-        <Navbar / >
-        <InputArea summarizeContent={this.summarizeContent}/>
-        <SummarizedText summary={this.state.summary}/>
+    return (
+      <div className="App pl-0">
+        <Navbar />
+        <InputArea summarizeContent={this.summarizeContent} />
+        <SummarizedText summary={this.state.summary} />
       </div>
     );
   }
--- src/Components/InputArea/InputArea.js
+++ src/Components/InputArea/InputArea.js
@@ -1,36 +1,57 @@
-import React, { Component } from 'react'
-import './InputArea.css'
+import React, { Component } from "react";
+import "./InputArea.css";
 
 export default class InputArea extends Component {
+  state = {
+    inputText: "",
+    numSentences: null
+  };
 
-  state={
-    inputText:''
-  }
-
-  handleSubmit=(event)=>{
+  handleSubmit = event => {
     event.preventDefault();
     this.props.summarizeContent(this.state);
-  }
+  };
 
-  handleChange=(event)=>{
+  handleChange = event => {
     this.setState({
-      inputText:event.target.value
-    })
-  }
-
+      [event.target.id]: event.target.value
+    });
+  };
 
   render() {
     return (
-    <div className="textArea ma5">
+      <div className="textArea ma5 mb0">
         <form className="pa4 black-80" onSubmit={this.handleSubmit}>
           <div>
-            <label htmlFor="comment" className="f3 b db mb2">Content</label>
-            <textarea onChange={this.handleChange} id="comment" className="comment db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2" aria-describedby="comment-desc"></textarea>
-            <small id="comment-desc" className="f6 black-60">Copy and paste the text you want to summarize in the above text box <a href="#" className="link underline black-80 hover-blue">more</a></small>
-         </div>
-         <button className="ma3 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib">Submit</button>
+            <label htmlFor="comment" className="f3 b db mb2">
+              Content
+            </label>
+            <textarea
+              onChange={this.handleChange}
+              id="inputText"
+              className="comment db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2"
+              aria-describedby="comment-desc"
+            />
+            <small id="comment-desc" className="f6 black-60">
+              Enter the number of sentences you want to summarize content into{" "}
+              <a href="#" className="link underline black-80 hover-blue">
+                more
+              </a>
+            </small>
+            <br />
+            <input
+              id="numSentences"
+              type="number"
+              name="quantity"
+              className="sentences ma3"
+              onChange={this.handleChange}
+            />
+          </div>
+          <button className="ma2 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib">
+            Submit
+          </button>
         </form>
-    </div>
-    )
+      </div>
+    );
   }
 }
